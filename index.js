5627bddbd4bce675946db1eba1762b43aa9ff03a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-15 13:20:18 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Copy and paste this on chrome console
+win1 = window.open("https://github.com/Prajwal-Koirala");
+timer1 = setInterval(function() {
+    win1.location.href = "https://github.com/Prajwal-Koirala"
+}, 1 * 1 * 1000);
