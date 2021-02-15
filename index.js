7f91e68d9e42fd651a84f82a016e65ca815cea83
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-15 13:26:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Copy and paste this on chrome console
-win1 = window.open("https://github.com/Prajwal-Koirala");
+win1 = window.open("https://github.com/complexorganizations/git-views-generator");
 timer1 = setInterval(function() {
-    win1.location.href = "https://github.com/Prajwal-Koirala"
+    win1.location.href = "https://github.com/complexorganizations/git-views-generator"
 }, 1 * 1 * 1000);
