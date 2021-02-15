e64a2bce7564a9db4a038d6996bc71871e2d6e4b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-15 13:34:41 -0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-// Copy and paste this on chrome console
-win1 = window.open("https://github.com/complexorganizations/git-views-generator");
-timer1 = setInterval(function() {
-    win1.location.href = "https://github.com/complexorganizations/git-views-generator"
-}, 1 * 1 * 1000);
