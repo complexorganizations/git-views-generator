ea119884e70f800d5ad9d12eb2c0de1e1d51439a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-04-20 06:57:20 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
@@ -13,7 +13,7 @@ func main() {
 		if err != nil {
 			log.Println(err)
 		}
-		body, err := ioutil.ReadAll(resp.Body)
+		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			log.Println(err)
 		}
